3b25fabbc5bf96997b0a7549623ba1b89ac35fc1
Alfredo Brizuela <vabrizuela2011@gmail.com>
2022-03-24 12:28:01 -0300

    modifique tabla

--- src/components/VistaCHtable.jsx
+++ src/components/VistaCHtable.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const VistaCHtable = () => {
   return (
-    <div>VistaCHtable</div>
+      
+    <div>
+        <h1>VistaCHtable</h1>
+        <p></p>
+    </div>
   )
 }
 
